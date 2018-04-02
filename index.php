eca6617f85cde41d48d1ec443b708ad5c5e9f365
Sergey <pmswga@gmail.com>
2018-04-02 21:04:41 +0300

    Подготовка
    - Установка Semantic
    - Настройка подключения к базе данных

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	
+	require_once "start.php";
+	
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Таксопарк</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/semantic.css">
+		<script src="js/jquery.js"></script>
+		<script src="js/semantic.js"></script>
+	</head>
+	<body>
+		<div class="ui grid">
+			<div class="row">
+			
+			</div>
+		</div>
+	</body>
+</html>
---
+++ start.php
@@ -0,0 +1,6 @@
+<?php
+	
+	$DB = new PDO("mysql:dbname=iep;host=127.0.0.1", "root", "");
+	$DB->exec("SET NAMES `utf8`");
+	
+?>
